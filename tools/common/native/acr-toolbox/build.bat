2368a5c8922100be62df496d5ddba985b4d1e9de
kadoka <tomiya3499@gmail.com>
2026-09-12 15:26:03 +0900

    Add acr-toolbox Windows build script

---
+++ tools/common/native/acr-toolbox/build.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+cd /d %~dp0
+if not exist ..\..\..\..\bin mkdir ..\..\..\..\bin
+if "%GOOS%"=="" for /f %%i in ('go env GOOS') do set GOOS=%%i
+if "%GOARCH%"=="" for /f %%i in ('go env GOARCH') do set GOARCH=%%i
+set OUT=..\..\..\..\bin\acr-toolbox
+if /I "%GOOS%"=="windows" set OUT=%OUT%.exe
+set CGO_ENABLED=0
+go build -trimpath -ldflags="-s -w" -o "%OUT%" .
+if errorlevel 1 exit /b 1
+echo built %OUT% (%GOOS%/%GOARCH%)
