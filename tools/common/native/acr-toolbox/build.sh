1fa15b78184e879d492bfdf1245b250b02fae418
kadoka <tomiya3499@gmail.com>
2026-09-12 15:25:56 +0900

    Add acr-toolbox Unix build script

---
+++ tools/common/native/acr-toolbox/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -eu
+cd "$(dirname "$0")"
+mkdir -p ../../../../bin
+OUT=../../../../bin/acr-toolbox
+GOOS=${GOOS:-$(go env GOOS)}
+GOARCH=${GOARCH:-$(go env GOARCH)}
+if [ "$GOOS" = "windows" ]; then OUT="${OUT}.exe"; fi
+CGO_ENABLED=0 GOOS="$GOOS" GOARCH="$GOARCH" go build -trimpath -ldflags="-s -w" -o "$OUT" .
+echo "built $OUT ($GOOS/$GOARCH)"
