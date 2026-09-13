cff55e38b5f2a6f68992bf40a306f598537228e7
kadoka <tomiya3499@gmail.com>
2026-09-13 11:25:32 +0900

    Add one-shot Go Windows build script

---
+++ tools/go/medium/affected-tests/build.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+cd /d %~dp0
+if not exist dist mkdir dist
+go test ./...
+if errorlevel 1 exit /b 1
+go build -trimpath -ldflags="-s -w" -o dist\affected-tests.exe .
+if errorlevel 1 exit /b 1
+echo built: %cd%\dist\affected-tests.exe
