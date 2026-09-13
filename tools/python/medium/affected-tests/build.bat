f59c815ecd609d11a3bf96a77bbe8be13803a97a
kadoka <tomiya3499@gmail.com>
2026-09-13 11:23:39 +0900

    Remove unnecessary Python build script

--- tools/python/medium/affected-tests/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d %~dp0
-if not exist dist mkdir dist
-set PYBIN=python
-where py >nul 2>nul && set PYBIN=py -3
-%PYBIN% -m zipapp . -m "affected_tests:main" -o dist\affected-tests.pyz -p "/usr/bin/env python3"
-if errorlevel 1 exit /b 1
-echo built: %cd%\dist\affected-tests.pyz
