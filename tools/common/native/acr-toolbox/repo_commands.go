cdd1790af4983597ccb14c94342879f736a728b6
kadoka <tomiya3499@gmail.com>
2026-09-15 18:39:05 +0900

    Centralize portable repository walking

--- tools/common/native/acr-toolbox/repo_commands.go
+++ tools/common/native/acr-toolbox/repo_commands.go
@@ -8,6 +8,8 @@ import (
 
 var ignoreDirs = map[string]bool{
     ".git": true,
+    ".hg": true,
+    ".svn": true,
     ".venv": true,
     "venv": true,
     "node_modules": true,
@@ -17,11 +19,14 @@ var ignoreDirs = map[string]bool{
     "dist": true,
     "__pycache__": true,
     ".godot": true,
+    ".idea": true,
+    ".vs": true,
+    "vendor": true,
 }
 
 var languageByExt = map[string]string{
     ".py": "Python", ".cs": "CSharp", ".go": "Go", ".c": "C", ".h": "C/C++",
-    ".cpp": "C++", ".cc": "C++", ".cxx": "C++", ".hpp": "C++", ".hh": "C++",
+    ".cpp": "C++", ".cc": "C++", ".cxx": "C++", ".hpp": "C++", ".hh": "C/C++",
     ".gd": "GDScript", ".rs": "Rust", ".js": "JavaScript", ".ts": "TypeScript", ".java": "Java",
 }
 
@@ -30,24 +35,39 @@ type fileInfo struct {
     Size int64
 }
 
-func walk(root string) ([]fileInfo, error) {
-    out := []fileInfo{}
+type walkResult struct {
+    Files      []fileInfo
+    ErrorCount int
+}
+
+func walkWithOptions(root string, includeIgnored bool) (walkResult, error) {
+    out := walkResult{Files: []fileInfo{}}
     err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
         if err != nil {
+            if path == root {
+                return err
+            }
+            out.ErrorCount++
             return nil
         }
         if d.IsDir() {
-            if path != root && ignoreDirs[strings.ToLower(d.Name())] {
+            if !includeIgnored && path != root && ignoreDirs[strings.ToLower(d.Name())] {
                 return filepath.SkipDir
             }
             return nil
         }
-        info, e := d.Info()
-        if e != nil {
+        info, infoErr := d.Info()
+        if infoErr != nil {
+            out.ErrorCount++
             return nil
         }
-        out = append(out, fileInfo{Path: path, Size: info.Size()})
+        out.Files = append(out.Files, fileInfo{Path: path, Size: info.Size()})
         return nil
     })
     return out, err
+}
+
+func walk(root string) ([]fileInfo, error) {
+    result, err := walkWithOptions(root, false)
+    return result.Files, err
 }
